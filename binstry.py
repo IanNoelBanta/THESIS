2e967fe08d472919a78013639303ddc4b776627e
Vince Villasor <78065232+vincekzir@users.noreply.github.com>
2024-04-11 16:13:51 +0800

    trytrytry hands count

---
+++ binstry.py
@@ -0,0 +1,90 @@
+from cvzone.HandTrackingModule import HandDetector
+import cv2
+import serial
+import time
+
+cap = cv2.VideoCapture(0)
+
+detector = HandDetector(staticMode=False, maxHands=5, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
+
+center = (int(640/2), int(480/2))
+centerBoxStart = (center[0]-50, center[1]-50)
+centerBoxEnd = (center[0]+50, center[1]+50)
+
+# arduino = serial.Serial(port = 'COM7', timeout=0)
+
+isXGood = False
+isYGood = False
+
+while True:
+    success, img = cap.read()
+
+    img = cv2.flip(img, 1)
+
+    hands, img = detector.findHands(img, draw=True, flipType=True)
+
+    if hands:
+        for handIndex, hand in enumerate(hands):
+            lmList = hand["lmList"]  
+            handNumber = (detector.fingersUp(hand)).count(1)
+
+            if handIndex == 0:  # Focus on the  first detected hand
+                if handNumber >= 1:  
+                    
+                    # Detected hands
+                    cv2.putText(img, f"{handIndex+1}: {handNumber} finger(s)", (lmList[8][0], lmList[8][1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2, cv2.LINE_AA)
+                    
+                    handFingerX, handFingerY = lmList[8][0:2]
+                    
+                    # finger plotting
+                    cv2.line(img , (handFingerX-50,handFingerY), (handFingerX+50,handFingerY), (0, 0, 255), 3, 3)
+                    cv2.line(img, (handFingerX,handFingerY-50), (handFingerX,handFingerY+50), (0, 0, 255), 3, 3)
+
+                   
+                    x = handFingerX
+                    y = handFingerY
+
+                    if x > centerBoxEnd[0]:
+                        xCommand = 'r'
+                    elif x < centerBoxStart[0]:
+                        xCommand = 'l' 
+                    else:
+                        xCommand = 's'
+
+                    if isXGood == False:
+                        print(xCommand)
+                        # arduino.write(str.encode(xCommand))
+
+                    if xCommand == 's':
+                        isXGood = True
+
+                        if y > centerBoxEnd[1]:
+                            yCommand = 'd'
+                        elif y < centerBoxStart[1]:
+                            yCommand = 'u' 
+                        else:
+                            yCommand = 's'
+
+                        if isYGood == False:
+                            print(yCommand)
+                            # arduino.write(str.encode(yCommand))
+
+                        if yCommand == 's':
+                            isYGood = True
+            else:
+                # Reset flags for next iteration for other hands
+                isXGood = False
+                isYGood = False
+
+    # center box plotting
+    cv2.rectangle(img, centerBoxStart, centerBoxEnd, (0, 255, 0), 10, 10)
+    # center dot
+    cv2.line(img, center, center, (255, 0, 0), 10)
+
+    cv2.imshow("Image", img)
+
+    if cv2.waitKey(1) == ord('x'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
