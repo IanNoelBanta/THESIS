f563171e9728c2de77cc5960863291fab6cc4298
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-04-29 14:43:09 +0800

    aaaaaaa

---
+++ NEWML.py
@@ -0,0 +1,92 @@
+import cv2
+import serial
+from ultralytics import YOLO
+
+model = YOLO("best500n.pt")
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+CAM_X_CENTER = 320
+CAM_Y_CENTER = 240
+TOLERANCE = 5
+CAM_LEFT_TOLERANCE = CAM_X_CENTER - TOLERANCE
+CAM_RIGHT_TOLERANCE = CAM_X_CENTER + TOLERANCE
+CAM_TOP_TOLERANCE = CAM_Y_CENTER - TOLERANCE
+CAM_BOTTOM_TOLERANCE = CAM_Y_CENTER + TOLERANCE
+
+xCommand = ""
+yCommand = ""
+isXGood = False
+isYGood = False
+
+
+while True:
+    success, frame = cap.read()
+    if not success:
+        print("Error reading frame")
+        break
+
+    results = model.track(frame, classes=[1], max_det=1, imgsz=640, vid_stride=1, stream_buffer=True, conf=0.8) # detect
+
+    if results[0]:
+        frame = results[0].plot() # plot lahat ng detections
+        targetX1 = int(results[0].boxes.xyxy.numpy()[0][0])
+        targetY1 = int(results[0].boxes.xyxy.numpy()[0][1])
+        targetX2 = int(results[0].boxes.xyxy.numpy()[0][2])
+        targetY2 = int(results[0].boxes.xyxy.numpy()[0][3])
+
+        targetY1Offset = targetY1 - 100
+
+        targetXCenter, targetYCenter = int((targetX1 + targetX2) / 2), int((targetY1 + targetY2) / 2)
+
+        targetCrosshairVerticalStart, targetCrosshairVerticalEnd = (targetXCenter, targetY1Offset - 10), (targetXCenter, targetY1Offset + 10) 
+        targetCrosshairHorizontalStart, targetCrosshairHorizontalEnd = (targetXCenter - 10, targetY1Offset), (targetXCenter + 10, targetY1Offset)
+
+        cv2.line(frame, targetCrosshairVerticalStart, targetCrosshairVerticalEnd, (0, 0, 255), 3) # crosshair vertical line
+        cv2.line(frame, targetCrosshairHorizontalStart, targetCrosshairHorizontalEnd, (0, 0, 255), 3) # crosshair horizontal line
+
+        if targetXCenter > CAM_RIGHT_TOLERANCE:
+            xCommand = 'l'
+        elif targetXCenter < CAM_LEFT_TOLERANCE:
+            xCommand = 'r' 
+        else:
+            xCommand = 'x'
+
+        if isXGood == False:
+            print(xCommand)
+            # arduino.write(str.encode(xCommand))
+
+        if xCommand == 'x':
+            isXGood = True
+
+            if targetY1Offset > CAM_BOTTOM_TOLERANCE:
+                yCommand = 'd'
+            elif targetY1Offset < CAM_TOP_TOLERANCE:
+                yCommand = 'u' 
+            else:
+                yCommand = 'y'
+
+            if isYGood == False:
+                print(yCommand)
+                # arduino.write(str.encode(yCommand))
+
+            if yCommand == 'y':
+                isYGood = True
+                # arduino.write(str.encode('z'))
+    else:
+        print("NO DETECTION - GO RIGHT")
+        # arduino.write(str.encode('r'))
+
+
+    cv2.line(frame, (CAM_LEFT_TOLERANCE, 0), (CAM_LEFT_TOLERANCE, 480), (255, 0, 0), 3) # left vertical line
+    cv2.line(frame, (CAM_RIGHT_TOLERANCE, 0), (CAM_RIGHT_TOLERANCE, 480), (255, 0, 0), 3) # right vertical line
+    cv2.line(frame, (0, CAM_TOP_TOLERANCE), (640, CAM_TOP_TOLERANCE), (0, 0, 255), 3) # top horizontal line
+    cv2.line(frame, (0, CAM_BOTTOM_TOLERANCE), (640, CAM_BOTTOM_TOLERANCE), (0, 0, 255), 3) # bottom horizontal line
+    cv2.imshow("Real-time Object Tracking", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
