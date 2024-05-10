69cefeba0562b2e5e229a8c067885f4d2f92c943
Vince Villasor <78065232+vincekzir@users.noreply.github.com>
2024-05-10 20:43:38 +0800

    Export Predict idk

--- Export.py
+++ Export.py
@@ -1,4 +1,4 @@
-# from ultralytics import YOLO
+from ultralytics import YOLO
 
 # # Load a model
 # model = YOLO('yolov8s.pt')  # load an official model
@@ -10,4 +10,8 @@
 
 from ultralytics.utils.checks import parse_requirements
 
-print(parse_requirements(package='ultralytics'))
+# Load the YOLOv8 model
+model = YOLO('best500n.pt')
+
+# Export the model to TFLite format
+model.export(format='tflite') # creates 'yolov8n_float32.tflite'
